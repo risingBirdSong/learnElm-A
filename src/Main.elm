b5bf1fdb9eae11ccb7691d348bd97d6884951df4
Peter <heregoingthere@outlook.com>
2021-05-25 21:41:46 -0700

    parsed json data including nested objects

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (Model)
+module Main exposing (..)
 
 -- https://elmprogramming.com/fetching-data-using-get.html
 
@@ -7,28 +7,69 @@ import Html.Events exposing (onClick)
 import Browser
 import Http
 import Debug
+import Json.Decode.Pipeline exposing (optional, optionalAt, required, requiredAt)
+import Dict
+import Json.Decode as Decode
+    exposing
+        ( Decoder
+        , decodeString
+        , field
+        , int
+        , list
+        , map3
+        , string
+        )
+type alias Model = {
+  mystr : String ,
+  myint : Int
+  }
+
+aaa = decodeString (field "id" int) "{ \"id\": 1 }"
 
 
-type alias Model = String
+bbb = decodeString string "\"Beanie\""
+
+ccc = decodeString (field "id" int) "{ \"id\": 1 }"
+ddd = decodeString (list (list int)) "[[1, 2], [4, 5]]"
+
+
+
 
 type alias Simple = {value : String, id : Int}
+type alias Nested = {mydata : Simple}
 
+simpleDecoder : Decoder Simple
+simpleDecoder = Decode.map2 Simple 
+  (field "value" string)
+  (field "id" int)
+
+nestedDecoder : Decoder Nested
+nestedDecoder = Decode.map Nested 
+ (field "mydata" simpleDecoder)
+
+simplea = Decode.decodeString simpleDecoder "{ \"value\":\"hello\",\"id\":1}"
+nesteda = Decode.decodeString nestedDecoder "{ \"mydata\" : { \"value\":\"hello\",\"id\":1}}"
+
+-- > Main.nesteda
+-- Ok { mydata = { id = 1, value = "hello" } }
+--     : Result Json.Decode.Error Main.Nested
 
 url : String
 url =
     "http://localhost:3000/simple"
 
+cat = "meow"
 
 type Msg
     = SendHttpRequest
     | DataReceived (Result Http.Error String)
 
-
 view : Model -> Html Msg 
 view model = 
   div [] [
     h1 [] [text "hello from connect"] , 
-    h3 [] [text model] ,
+    h3 [] [text model.mystr] ,
+    h3 [] [text (String.fromInt model.myint)] ,
     button [onClick SendHttpRequest] [text "click to fire off request"] 
   ]
 
@@ -46,7 +87,7 @@ update msg model =
       (model, queryForSimple )
     DataReceived (Ok jsonlist) ->
 
-      (jsonlist, Cmd.none)
+      ({mystr = jsonlist, myint = 2}, Cmd.none)
     DataReceived (Err e) ->
       (model, Cmd.none)
 
@@ -54,8 +95,9 @@ update msg model =
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \flags -> ( "start", Cmd.none )
+        { init = \flags -> ( {mystr = "start", myint = 1}  , Cmd.none )
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
